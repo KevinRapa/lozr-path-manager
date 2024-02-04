763acdef8a814e4e0b1f1a71a4ea84bbd27eb4e9
kevjrapa@gmail.com <kevjrapa@gmail.com>
2024-02-04 13:38:02 -0500

    Radio buttons update paths now

--- src/mapper.tsx
+++ src/mapper.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {CFG} from './util/AllRooms';
-import {useState,useRef} from 'react';
+import {useState,useRef,useEffect} from 'react';
 import {PathDisplay} from './components/PathDisplay';
 import {FromToModule} from './components/FromToModule';
 import {AdultChildButtons,LinkState} from './components/AdultChildButtons';
@@ -86,7 +86,8 @@ export function Mapper()
 	                    } as MapperState)
 	);
 	const [foundPaths, setFoundPaths] = useState<string[][]>([]);
-	const linkState = useRef<LinkState>("CHILD");
+	const [linkState, setLinkState] = useState<LinkState>("CHILD");
+	const [fromTo, setFromTo] = useState<[string, string]|null>(null);
 
 	const linkFunction = (fromId: string, toId: string) => {
 		setMapperState(getUpdatedDoors([[fromId, toId]], mapperState));
@@ -117,9 +118,16 @@ export function Mapper()
 		setMapperState(newState);
 	}
 
-	const findFunction = (fromId:string, toId:string) => {
-		let isChild: boolean = linkState.current === "CHILD";
-		let allStarts: string[] = [fromId].concat(
+	const findFunction = () => {
+		if (!fromTo) {
+			return;
+		}
+
+		let fromRoom: string = fromTo[0];
+		let toRoom: string = fromTo[1];
+
+		let isChild: boolean = linkState === "CHILD";
+		let allStarts: string[] = [fromRoom].concat(
 			_.keys(mapperState.additionalBegin)
 		         .filter((roomId: string) => {
 				let warpMethod: string = mapperState.additionalBegin[roomId];
@@ -132,17 +140,16 @@ export function Mapper()
 		);
 		let allPaths: string[][] = [];
 
-		console.log(`Find from any ${allStarts} to ${toId}`);
+		console.log(`Find from any ${allStarts} to ${toRoom}`);
 
 		for (let startId of allStarts) {
-			if (!startId || !toId) {
-				console.log(`ERROR: null [${startId}, ${toId}]`);
+			if (!startId) {
 				continue;
 			}
 
 			let foundPaths: string[][] = separatePathTree(findAllPaths(mapperState.roomToDoors,
 			                                              mapperState.doorToDoor,
-			                                              startId, toId, isChild));
+			                                              startId, toRoom, isChild));
 			allPaths.push(...foundPaths);
 		}
 
@@ -150,6 +157,12 @@ export function Mapper()
 		console.log(allPaths);
 		setFoundPaths(allPaths);
 	};
+
+	const findButtonHandler = (fromId: string, toId: string) => {
+		setFromTo([fromId, toId]);
+	}
+
+	useEffect(findFunction, [linkState, fromTo]);
 
 	// Exclude rooms that shouldn't be known about, like "Kakariko Village Backyard"
 	let selectableRoomMap: Record<string, string> =
@@ -159,8 +172,8 @@ export function Mapper()
 	console.log(mapperState);
 
 	return <>
-		<AdultChildButtons initialState={linkState.current}
-		                   onChange={(state:LinkState)=>{linkState.current=state}}
+		<AdultChildButtons initialState={linkState}
+		                   onChange={setLinkState}
 		/>
 		<FromToModule idToNameMapFrom={mapperState.unlinkedOwls}
 		              idToNameMapTo={_.omit(CFG.areas, CFG.no_add)}
@@ -179,7 +192,7 @@ export function Mapper()
 		/>
 		<FromToModule idToNameMapFrom={selectableRoomMap}
 		              idToNameMapTo={selectableRoomMap}
-		              onClick={findFunction}
+		              onClick={findButtonHandler}
 		              buttonTitle={"FIND"}
 		/>
 		<button onClick={()=>saveJson(JSON.stringify(mapperState), 'lozr-cfg.json')}>
